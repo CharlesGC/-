ec03be85e2f3c2b78a4e0a3806e26767d83dd886
guochao <1432997325@qq.com>
2018-10-23 15:10:35 +0800

    city-components

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hotCities="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="letterChange"></city-alphabet>
   </div>
 </template>
 
@@ -18,8 +18,9 @@ export default {
   components: {CityAlphabet, CityList, CitySearch, CityHeader},
   data () {
     return {
-      cities: [],
-      hotCities: []
+      cities: {},
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -27,14 +28,15 @@ export default {
       axios.get('/api/city.json')
         .then(this.getCityInfoSucc)
     },
+    letterChange (l) {
+      this.letter = l
+    },
     getCityInfoSucc (res) {
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
         this.cities = data.cities
         this.hotCities = data.hotCities
-        console.log(data)
-        console.log(this.cities, this.hotCities, '+++++++++++++')
       }
     }
   },
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,14 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+    <li class="item"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+        @click="letterClick"
+        @touchstart="touchStart"
+        @touchmove="touchMove"
+        @touchend="touchEnd"
+    >{{key}}</li>
   </ul>
 </template>
 
@@ -9,6 +17,51 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    letterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    touchStart () {
+      this.touchStatus = true
+    },
+    touchMove (e) {
+      if (this.touchStatus === true) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const startY = this.startY
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    touchEnd () {
+      this.touchStatus = false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
   }
 }
 </script>
